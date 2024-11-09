a3156e83fd76056fec37e9677356c8b76c8993df
Ronan Graham <29425751+Ro4052@users.noreply.github.com>
2024-11-09 11:41:36 +0000

    refine button states

--- components/primitives/Button/Button.tsx
+++ components/primitives/Button/Button.tsx
@@ -1,29 +1,42 @@
 import { twMerge } from "tailwind-merge";
+import { Spinner } from "./Spinner";
 
 type Variant = "primary";
 
 const variantClass: Record<Variant, string> = {
-  primary: "bg-green-400 hover:bg-green-500 disabled:bg-green-400",
+  primary:
+    "bg-green-400 hover:bg-green-500 disabled:bg-green-400 active:bg-green-600",
 };
 
 interface ButtonProps {
   children: string;
   disabled?: boolean;
+  loading?: boolean;
   type: "submit";
   variant: Variant;
 }
 
-export const Button = ({ children, disabled, type, variant }: ButtonProps) => (
-  // TODO: Loading styles
+export const Button = ({
+  children,
+  disabled,
+  loading,
+  type,
+  variant,
+}: ButtonProps) => (
   <button
     className={twMerge(
-      // TODO: Review focus/active states - outline should be for focus, darker for active
-      "rounded-md p-2 font-semibold text-stone-800 focus:outline focus:outline-2 focus:outline-offset-2 focus:outline-blue-500 disabled:opacity-70 disabled:focus:outline-0",
+      "rounded-md p-2 font-semibold text-stone-800 focus:outline focus:outline-2 focus:outline-offset-2 focus:outline-blue-500 disabled:opacity-60 disabled:focus:outline-0",
       variantClass[variant]
     )}
     type={type}
-    disabled={disabled}
+    disabled={disabled || loading}
   >
-    {children}
+    {loading ? (
+      <div className="flex justify-center">
+        <Spinner />
+      </div>
+    ) : (
+      children
+    )}
   </button>
 );
