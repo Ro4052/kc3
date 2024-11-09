a2dc56b02de92e9989cd8159222eb101f8b445d8
Ronan Graham <29425751+Ro4052@users.noreply.github.com>
2024-11-09 12:03:47 +0000

    basic error handling on tree page

--- app/tree/[dog]/page.tsx
+++ app/tree/[dog]/page.tsx
@@ -2,11 +2,11 @@ import { Dog } from "@/models/dog";
 import { baseURLHeader } from "@/models/headers";
 import { headers } from "next/headers";
 
-const getDog = async (id: string): Promise<Dog> => {
+const getDog = async (id: string): Promise<Dog | null> => {
   const baseURL = (await headers()).get(baseURLHeader);
   const response = await fetch(`${baseURL}/api/tree/dog/${id}`);
   if (!response.ok) {
-    throw new Error("Error fetching data");
+    return null;
   }
 
   return await response.json();
@@ -21,14 +21,18 @@ export const generateMetadata = async ({ params }: FamilyTreeProps) => {
   const dog = await getDog(dogId);
 
   return {
-    title: `kc3 | ${dog.name}`,
-    description: `kc3 - ${dog.name} Kennel Club family tree`,
+    title: `kc3 | ${dog ? dog.name : "Family tree"}`,
+    description: `kc3 - ${dog ? `${dog.name}` : ""}Kennel Club family tree`,
   };
 };
 
 export default async function FamilyTree({ params }: FamilyTreeProps) {
   const dogId = (await params).dog;
   const dog = await getDog(dogId);
+
+  if (!dog) {
+    return "Couldn't find dog profile :(";
+  }
 
   return (
     <div className="grid">
